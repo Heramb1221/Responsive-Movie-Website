e2ad987cb187f7a62f65a647113cec18b71b6937
Heramb Chaudhari <169079872+Heramb1221@users.noreply.github.com>
2024-10-08 02:27:56 +0530

    Add files via upload

---
+++ JS/main.js
@@ -0,0 +1,62 @@
+document.addEventListener('DOMContentLoaded', function () {
+  var swiper = new Swiper(".home", {
+      spaceBetween: 30,
+      centeredSlides: true,
+      autoplay: {
+          delay: 2500,
+          disableOnInteraction: false,
+      },
+      pagination: {
+          el: ".swiper-pagination",
+          clickable: true,
+      }
+  });
+});
+
+// Swiper for the coming section
+var comingSwiper = new Swiper(".ccontainer", {
+  spaceBetween: 20,
+  loop: true,
+  autoplay: {
+    delay: 55000, // Reduced autoplay delay
+    disableOnInteraction: false,
+  },
+  centeredSlides: true,
+  pagination: {
+    el: ".swiper-pagination", // Added pagination for the coming section
+    clickable: true,
+  },
+  breakpoints: {
+    0: {
+      slidesPerView: 2,
+    },
+    568: {
+      slidesPerView: 3,
+    },
+    768: {
+      slidesPerView: 4,
+    },
+    992: {
+      slidesPerView: 5,
+    },
+  },
+});
+
+let header = document.querySelector('header');
+let menu = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+window.addEventListener('scroll', () =>{
+  header.classList.toggle('shadow', window.scrollY > 0)
+})
+
+menu.onclick = () => {
+  menu.classList.toggle('bx-x');
+  navbar.classList.toggle('active');
+}
+
+window.onscroll = ()  => {
+  menu.classList.remove('bx-x');
+  navbar.classList.remove('active'); 
+}
+
